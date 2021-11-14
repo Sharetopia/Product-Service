7267acc9005851582cf094922123bedf8b6908be
Niklas Schildhauer <ns162@hdm-stuttgart.de>
2021-11-14 15:00:29 +0100

    init spring boot

---
+++ src/main/kotlin/de/sharetopia/productservice/ProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package de.sharetopia.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductServiceApplication>(*args)
+}
