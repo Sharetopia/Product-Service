5763885b65679d223c4c7a412c4df5033b4d58e7
js2203 <65500109+js2203@users.noreply.github.com>
2022-02-10 18:20:22 +0100

    updated usermodel

--- src/main/kotlin/de/sharetopia/productservice/product/dto/UserDTO.kt
+++ src/main/kotlin/de/sharetopia/productservice/product/dto/UserDTO.kt
@@ -1,6 +1,10 @@
 package de.sharetopia.productservice.product.dto
 
 data class UserDTO(var profilePictureURL: String = "",
-                   var name: String = "",
-                   var postalCode: String = ""
+                   var forename: String = "",
+                   var surname: String = "",
+                   var address: String = "",
+                   var city: String = "",
+                   var rating: String = "",
+                   var postalCode: String = "",
 )
